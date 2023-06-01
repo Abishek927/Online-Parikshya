52f05fd948712ff1a279faa0503bc645e74c68be
Abishek <khadkaabishek94@gmail.com>
2023-06-01 23:00:20 +0545

    cors issue solve

--- src/main/java/com/online/exam/config/MyConfig.java
+++ src/main/java/com/online/exam/config/MyConfig.java
@@ -22,6 +22,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -41,7 +43,7 @@ public class MyConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
-                .cors().disable()
+                .cors().and()
                 .authorizeHttpRequests()
                 .requestMatchers(AppConstant.PUBLIC_URLS).permitAll()
 
@@ -71,5 +73,15 @@ public class MyConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
