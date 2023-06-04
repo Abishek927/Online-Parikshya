fbdb88edd2119cf4b707b78c750973f435a56fb4
Abishek <khadkaabishek94@gmail.com>
2023-06-04 21:03:20 +0545

    changes in userRepo

--- src/main/java/com/online/exam/repo/UserRepo.java
+++ src/main/java/com/online/exam/repo/UserRepo.java
@@ -13,8 +13,8 @@ public interface UserRepo extends JpaRepository<User,Long> {
 
     User findByUserEmail(String userEmail);
     User findByUserName(String userName);
-    @Query(value = "select u.user_id,u.user_email,u.user_name,u.user_status,u.user_password,u.user_contact_number,u.user_deleted,u.is_enabled,u.user_date_of_birth,u.user_gender,u.user_roll_no from user_table u where u.user_status=?1",nativeQuery = true)
+    @Query(value = "select u.user_id,u.user_email,u.user_name,u.user_status,u.user_password,u.user_contact_number,u.deleted,u.is_enabled,u.user_date_of_birth,u.user_gender,u.user_roll_no from user_table u where u.user_status=?1",nativeQuery = true)
     List<User> findPendingTeacher(String status);
-    @Query(value = "select u.user_id,u.user_email,u.user_name,u.user_status,u.user_password,u.user_contact_number,u.user_deleted,u.is_enabled,u.user_date_of_birth,u.user_gender,u.user_roll_no from user_table u where u.user_status=?1",nativeQuery = true)
+    @Query(value = "select u.user_id,u.user_email,u.user_name,u.user_status,u.user_password,u.user_contact_number,u.deleted,u.is_enabled,u.user_date_of_birth,u.user_gender,u.user_roll_no from user_table u where u.user_status=?1",nativeQuery = true)
     List<User> findPendingStudent(String status);
 }
